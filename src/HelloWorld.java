e458ca3800aa829cd58de05aa57b93bf00b5ac93
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-22 17:41:00 -0500

    feat: refactored just a bit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("I want to say hello to the world!");
         System.out.print("Hello, ");
         System.out.print("World!");
     }
