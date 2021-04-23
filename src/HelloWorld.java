29b8a6ebeef529bfc689d34d243db6dbcea16a3c
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-23 13:56:49 -0500

    feat: worked on syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,55 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("I want to say hello to the world!");
-        System.out.print("Hello, ");
-        System.out.print("World!");
+//        System.out.println("I want to say hello to the world!");
+//        System.out.print("Hello, ");
+//        System.out.print("World!");
+        int myFavoriteNumber = 15;
+        System.out.println(myFavoriteNumber);
+        String myString = "blah blah";
+        System.out.println(myString);
+//        myString = 'g'; //cant re-assign a variable to a different data type
+//        myString = 42; //same thing as above^^
+        float myNumber;
+//        System.out.print(myNumber); //can't run an empty variable!
+//        myNumber = 3.14; //can't assign a value that big to long, double is required
+//        myNumber = 123L;
+//        System.out.println(myNumber);
+//        myNumber = 123;
+//        System.out.println(myNumber);
+//        myNumber = 3.14F;
+//        System.out.println(myNumber);
+//        myNumber = (float) 3.14;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int y = 5;
+//        System.out.println(++y);
+//        System.out.println(y);
+
+        // String class = "Hello"; class is a reserved word and cant be used
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three); cant run because int has a string assigned to it
+
+//        int three2 = (int) "three";  //same thing as line 36^
+//        int z = 4;
+//        z += 5;
+//        System.out.println(z);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+//        x /= y;
+        System.out.println(x);
+        y -= x;
+        System.out.println(y);
+        int max = Integer.MAX_VALUE;
+        System.out.println(max);
     }
 }
