e9a878e2e5c8cb35eb54f3c568427fcca01dd3b1
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-28 10:34:45 -0500

    feat: done with #5 bonus

--- src/HighLow.java
+++ src/HighLow.java
@@ -11,7 +11,7 @@ public class HighLow {
         int randomNum = (int) (Math.random() * 101);
         System.out.println("My random generator has generated a number between 1 and 100");
         System.out.println(randomNum);
-        System.out.println("Please guess a number between 1 and 100");
+        System.out.println("Please guess a number between 1 and 100, keep in mind you only have 10 guesses >:)");
         Scanner sc = new Scanner(System.in);
         double userNum = sc.nextDouble();
         if (userNum < 0 || userNum > 100) {
@@ -20,16 +20,24 @@ public class HighLow {
         }
 
         boolean highLow = true;
-
+        int guessAttempts = 1;
         while (highLow) {
-            if (userNum < randomNum) {
+            if (userNum < randomNum && guessAttempts <= 10) {
+                System.out.printf("Guess #%d\n", guessAttempts);
                 System.out.println("Go higher! Input another number!");
+                guessAttempts += 1;
                 userNum = sc.nextDouble();
-            } else if (userNum > randomNum) {
+            } else if (userNum > randomNum && guessAttempts <= 10) {
+                System.out.printf("Guess #%d\n", guessAttempts);
                 System.out.println("Go lower! Input another number!");
+                guessAttempts += 1;
                 userNum = sc.nextDouble();
             } else if (userNum == randomNum) {
                 System.out.println("Correcto guesso!");
+                highLow = false;
+            }
+            if (guessAttempts == 11) {
+                System.out.println("So sorry, you're out of attempts! Try again later! >:)");
                 highLow = false;
             }
         }
