e80ec2d06c9550515820046ea6e1ff577072e840
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-29 17:40:56 -0500

    feat: starting oop exercises

--- src/Person.java
+++ src/Person.java
@@ -1,15 +1,32 @@
 public class Person {
 
+    private String personName;
 
-//    public String getName(name){
-////TODO: return the person's name
-//
-//    }
-//
-//    public void setName(String name){
-////TODO: change the name property to the passed value
-//    }
-//    public void sayHello(){
-////TODO: print a message to the console using the person's name
-//    }
+    public String getName(){
+//TODO: return the person's name
+        return personName = "Unknown";
+    }
+
+    public String setName(String name){
+//TODO: change the name property to the passed value
+        return personName = name;
+    }
+
+    public String sayHello(){
+//TODO: print a message to the console using the person's name
+        return "Hey how's it going " + personName + "?";
+    }
+
+    public static void main(String[] args) {
+//        Person alex = new Person();
+//        alex.setName("Alex");
+//        System.out.println("alex.personName = " + alex.personName);
+//        System.out.println("alex.sayHello() = " + alex.sayHello());
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+    }
 }
