10adad646ec5378a4378f2a5d9d236e8d5c294d0
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-04-29 11:04:30 -0500

    feat: quiz

---
+++ src/Help.java
@@ -0,0 +1,16 @@
+public class Help {
+    public static void main(String[] args) {
+//        int x = 5;
+//        System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7);
+
+//        int x = 5 * 4 % 3;
+//        System.out.println(x);
+
+//        int bleh = "45";
+//        System.out.println(bleh);
+
+        String blah = "bleh";
+        blah += 45;
+        System.out.println(blah);
+    }
+}
