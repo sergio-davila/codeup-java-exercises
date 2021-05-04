56f0e7f91959bf0744c146c31430a53d45467322
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-04 11:10:06 -0500

    feat: working on movies application

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,21 @@
 package movies;
 
 public class Movie {
+    private String name;
+    private String category;
+
+    public Movie(String movieName, String movieCategory) {
+        this.name = movieName;
+        this.category = movieCategory;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String setName(String movieName){
+        return name = movieName;
+    }
+
+
 }
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,12 @@
+package movies;
+
+import util.Input;
+
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
