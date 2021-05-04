b17a1c5a5e89a23af6525a91be821b8611d50c66
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-04 10:24:42 -0500

    feat: done with name generator

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,15 +1,17 @@
 public class ServerNameGenerator {
 
-    String[] adjectives = {"super", "mega", "ultimate", "best", "great", "crazy", "dedicated", "motivated", "galactic", "powerful"};
-    String[] nouns = {"galaxy", "universe", "forest", "city", "jungle", "desert", "cave", "mountain", "park", "island"};
+    public static String[] adjectives = {"super", "mega", "ultimate", "best", "great", "crazy", "dedicated", "motivated", "galactic", "powerful"};
+    public static String[] nouns = {"galaxy", "universe", "forest", "city", "jungle", "desert", "cave", "mountain", "park", "island"};
 
-    public String serverName() {
-        int randomNum = (int) (Math.random() * 11);
-        int randomNum2 = (int) (Math.random() * 11);
-        return adjectives[randomNum] + nouns[randomNum2];
+    public static void serverName() {
+        int randomNum = (int) (Math.random() * 10);
+        int randomNum2 = (int) (Math.random() * 10);
+        String newServer = adjectives[randomNum] + "-" + nouns[randomNum2];
+        System.out.println("Here is your server name: " + newServer);
     }
 
     public static void main(String[] args) {
 
+        serverName();
     }
 }
