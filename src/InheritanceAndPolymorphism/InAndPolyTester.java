efe872881f8de9e53c6a43fe3019741a5fa942dd
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-04 14:35:06 -0500

    feat: working on polymorphism lec

--- src/InheritanceAndPolymorphism/InAndPolyTester.java
+++ src/InheritanceAndPolymorphism/InAndPolyTester.java
@@ -2,6 +2,8 @@ package InheritanceAndPolymorphism;
 
 import oopLecture.Person;
 
+import java.util.Locale;
+
 public class InAndPolyTester {
 
     public static void main(String[] args) {
@@ -30,5 +32,36 @@ public class InAndPolyTester {
         bus.honkHorn();
         bus.setNumberOfOccupants(2);
         System.out.println("Shmovin in a bus at " + bus.getMaxSpeed() + "mph with " + bus.getNumberOfOccupants() + " occupants inside!");
+
+        Jet fastJet = new Jet(2000);
+        fastJet.setNumberOfOccupants(1);
+        fastJet.start();
+        System.out.println("ZOOMIN in a jet at " + fastJet.getMaxSpeed() + "mph with " + bus.getNumberOfOccupants() + " occupants inside!");
+        System.out.println();
+        Vehicle fasterBus = spawnVehicle("bus",120);
+        fasterBus.start();
+
+        Vehicle slowerJet = spawnVehicle("jet", 700);
+        slowerJet.start();
+    }
+
+
+
+    public static Vehicle spawnVehicle(String vehicleType, int maxSpeed) {
+        Vehicle vehicle = null;
+
+        switch (vehicleType.toLowerCase()) {
+            case "vehicle":
+                vehicle = new Vehicle(maxSpeed);
+                break;
+            case "bus":
+                vehicle = new Bus(maxSpeed);
+                break;
+            case "jet":
+                vehicle = new Jet(maxSpeed);
+                break;
+        }
+
+        return vehicle;
     }
 }
--- src/InheritanceAndPolymorphism/Jet.java
+++ src/InheritanceAndPolymorphism/Jet.java
@@ -1,6 +1,15 @@
 package InheritanceAndPolymorphism;
 
 public class Jet extends Vehicle {
+    private int altitude;
 
+    public Jet(int maxSpeed) {
+        super(maxSpeed);
+        this.altitude = 0;
+    }
+
+    public void start() {
+        System.out.println("... igniting jet engines!");
+    }
 
 }
