f66d6b0a8e5de738c75f6c94b0c94cabb5346ea1
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-05 10:35:25 -0500

    feat: finishing movie app

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -6,21 +6,70 @@ public class MoviesApplication {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int userInput;
         Movie[] movies = MoviesArray.findAll();
-        System.out.println("Would you like to see the list of all the movies, or would you like to see them by genre?");
-        System.out.println("Please input one of the following: 0 to not display anything.");
-        System.out.println("1 to view all movies.");
-        System.out.println("2 to view movies in the drama category.");
-        System.out.println("3 to view movies in the musical category.");
-        System.out.println("4 to view movies in the sci-fi category.");
-        System.out.println("5 to view movies in the animated category.");
 
-        String yOrN = sc.next();
-        if (yOrN.equalsIgnoreCase("yes")) {
-            for (Movie movie : movies) {
-                System.out.println();
+
+
+        boolean looper = true;
+        while (looper) {
+            System.out.println("Time to see a list of movies!");
+            System.out.println("Please input one of the following: 0 to not display anything.");
+            System.out.println("1 to view all movies.");
+            System.out.println("2 to view movies in the drama category.");
+            System.out.println("3 to view movies in the musical category.");
+            System.out.println("4 to view movies in the sci-fi category.");
+            System.out.println("5 to view movies in the animated category.");
+            userInput = sc.nextInt();
+            if (userInput == 0) {
+                looper = false;
+            } else if (userInput == 1) {
+                for (Movie movie : movies) {
+                    System.out.println(movie.getName() + ": " + movie.getCategory());
+                }
+            } else if (userInput == 2) {
+                for (Movie movie : movies) {
+                    if (movie.getCategory().equals("drama")) {
+                        System.out.println(movie.getName() + ": " + movie.getCategory());
+                    }
+                }
+            } else if (userInput == 3) {
+                for (Movie movie : movies) {
+                    if (movie.getCategory().equals("musical")) {
+                        System.out.println(movie.getName() + ": " + movie.getCategory());
+                    }
+                }
+            } else if (userInput == 4) {
+                for (Movie movie : movies) {
+                    if (movie.getCategory().equals("sci-fi")) {
+                        System.out.println(movie.getName() + ": " + movie.getCategory());
+                    }
+                }
+            } else if (userInput == 5) {
+                for (Movie movie : movies) {
+                    if (movie.getCategory().equals("animated")) {
+                        System.out.println(movie.getName() + ": " + movie.getCategory());
+                    }
+                }
+            } else {
+                System.out.println("Try again.");
+            }
+
+            System.out.println("Would you like to see a different movie list? Answer with yes or no please.");
+            boolean looper2 = true;
+            String YorN = sc.next();
+            while (looper2) {
+                if (YorN.equalsIgnoreCase("yes")) {
+                    System.out.println("MORE MOVIES");
+                } else if (YorN.equalsIgnoreCase("no")) {
+                    System.out.println("NO MORE MOVIES");
+                    looper = false;
+                } else {
+                    System.out.println("Try again.");
+                }
             }
         }
+
 
 
     }
