8b0a0bc1c30f50eddc8de7794344a742aadc5780
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-07 10:45:15 -0500

    feat: working on collections lecture

---
+++ src/CollectionsPractice.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class CollectionsPractice {
+
+    ArrayList<String> names1 = new ArrayList<>(Arrays.asList("Justin", "Douglas", "Kenneth"));
+
+//    public static String[] upperLastElement(String[] array) {
+//        String[] newArray = Arrays.copyOf(array, (array.length));
+//        newArray[array.length - 1] = newArray[array.length - 1].toUpperCase();
+//        return newArray;
+//    }
+//
+
+//    public static ArrayList<String> upperLastElement(ArrayList<String> names) {
+//
+//    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
