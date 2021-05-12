f5051d5d85ef17492740f673f90e53922cd0984a
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-12 13:28:34 -0500

    feat: refactored a few apps

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class GradesApplication{
 
 
+
     public static void main(String[] args) {
         Map<String, Student> students = new HashMap<>();
         Student Vanessa = new Student("Vanessa");
