a69694247ebdd9ab4afa51633b5b9c8f986d1c23
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-29 23:54:19 -0500

    feat: still working on beans exercise

--- src/JavaAdListerPaste/ServletScratch5.java
+++ src/JavaAdListerPaste/ServletScratch5.java
@@ -17,7 +17,19 @@ public class ServletScratch5 {
 //            album2.setSales(2.7F);
 //            album2.setGenre("Punk");
 //
+//  Quote quote1 = new Quote(1, "If you're going through hell, keep going.", "Winston Churchill");
+//    Quote quote2 = new Quote();
 //
+//        quote2.setId(2);
+//        quote2.setContent("Everything should be made as simple as possible, but not simple.");
+//        quote2.setAuthor("Albert Einstein");
+//
+//    Author author1 = new Author(1, "Douglas", "Adams");
+//    Author author2 = new Author();
+//
+//        author2.setId(2);
+//        author2.setFirstName("Kurt");
+//        author2.setLastName("Vonnegut");
 //        }
 //    }
 }
