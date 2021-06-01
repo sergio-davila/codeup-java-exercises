9156d6161c27f0ba33d0c658732ea42a9b443017
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-01 16:35:07 -0500

    feat: refactored serv scratch 7

--- src/JavaAdListerPaste/ServletScratch7.java
+++ src/JavaAdListerPaste/ServletScratch7.java
@@ -23,4 +23,28 @@ public class ServletScratch7 {
 //        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
 //    }
 //}
+
+//    <%--
+//  Created by IntelliJ IDEA.
+//  User: sergiodavila
+//  Date: 6/1/21
+//  Time: 11:08 AM
+//  To change this template use File | Settings | File Templates.
+//--%>
+//<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+//<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+//<html>
+//<head>
+//    <title>Ads</title>
+//</head>
+//<body>
+//
+//<c:forEach var="ad" items="${ads}">
+//    <div class="ad">
+//    <h2>${ad.title}</h2>
+//    <p>${ad.description}</p>
+//    </div>
+//</c:forEach>
+//</body>
+//</html>
 }
