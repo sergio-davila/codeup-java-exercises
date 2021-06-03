b5664f415a7a27456fb1bbbc06e82de1f435fd56
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-06-02 23:44:24 -0500

    feat: worked on cookies exercise

---
+++ src/JavaAdListerPaste/ServletScratch9.java
@@ -0,0 +1,32 @@
+package JavaAdListerPaste;
+
+public class ServletScratch9 {
+//    import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+//public class LoginServlet extends HttpServlet {
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        if (request.getSession().getAttribute("user") != null) {
+//            response.sendRedirect("/profile");
+//            return;
+//        }
+//        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+//    }
+//
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//        boolean validAttempt = username.equals("admin") && password.equals("password");
+//
+//        if (validAttempt) {
+//            request.getSession().setAttribute("user", username);
+//            response.sendRedirect("/profile");
+//        } else {
+//            response.sendRedirect("/login");
+//        }
+//    }
+//}
+}
